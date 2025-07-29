184a2c5ca757834c3e6af4b671fc9942ea9acd0a
Jack Tolfree <96070967+jato-c8y@users.noreply.github.com>
2025-07-29 14:48:05 +0100

    Adds config option for subscription mode

--- subscribe.go
+++ subscribe.go
@@ -56,6 +56,7 @@ func (c *client) createConsumer(topic, topicsPattern, subscriptionType, initialP
 		SubscriptionName:            subscriptionName,
 		Type:                        stringToSubscriptionType(subscriptionType),
 		SubscriptionInitialPosition: stringToSubscriptionInitialPosition(initialPosition),
+		SubscriptionMode:            c.conf.subscriptionMode,
 	}
 
 	consumer, err := c.pulsarClient.Subscribe(opts)
